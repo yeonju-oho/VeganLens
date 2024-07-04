20112aa3e203c80b15700c3842a1e0b895251ca2
yeonju-oho <yeonju.oho@gmail.com>
2024-07-05 03:09:02 +0900

    카메라 동작시켜서 ocr 인식후 내용 팝업 기능 추가

--- app/src/main/java/com/example/veganlens/startPages/VeganReasonFragment.kt
+++ app/src/main/java/com/example/veganlens/startPages/VeganReasonFragment.kt
@@ -16,6 +16,9 @@ import com.example.veganlens.databinding.FragmentVeganReasonBinding
 class VeganReasonFragment : Fragment() {
 
     private lateinit var binding: FragmentVeganReasonBinding
+    private lateinit var cardViews: List<View>
+    private lateinit var textViews: List<TextView>
+    private lateinit var isSelectedArray: BooleanArray
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,37 +31,58 @@ class VeganReasonFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setupCardView(binding.cardViewHealth, R.id.cardTextHealth)
-        setupCardView(binding.cardViewMeat, R.id.cardTextMeat)
-        setupCardView(binding.cardViewAnimal, R.id.cardTextAnimal)
-        setupCardView(binding.cardViewReligion, R.id.cardTextReligion)
-        setupCardView(binding.cardViewEnvironment, R.id.cardTextEnvironment)
-        setupCardView(binding.cardViewWeight, R.id.cardTextWeight)
-        setupCardView(binding.cardViewFamine, R.id.cardTextFamine)
+        cardViews = listOf(
+            binding.cardViewHealth,
+            binding.cardViewMeat,
+            binding.cardViewAnimal,
+            binding.cardViewReligion,
+            binding.cardViewEnvironment,
+            binding.cardViewWeight,
+            binding.cardViewFamine
+        )
 
+        textViews = listOf(
+            binding.cardTextHealth,
+            binding.cardTextMeat,
+            binding.cardTextAnimal,
+            binding.cardTextReligion,
+            binding.cardTextEnvironment,
+            binding.cardTextWeight,
+            binding.cardTextFamine
+        )
+
+        isSelectedArray = BooleanArray(cardViews.size)
+
+        // 카드뷰 설정
+        for (i in cardViews.indices) {
+            setupCardView(cardViews[i], textViews[i], i)
+        }
+
+        // 다음 버튼 설정
         binding.buttonNext.setOnClickListener {
-            findNavController().navigate(R.id.action_VeganReasonFragment_to_VeganLevelFragment)
+            if (isSelectedArray.any { it }) {
+                findNavController().navigate(R.id.action_VeganReasonFragment_to_VeganLevelFragment)
+            }
         }
     }
 
-    private fun setupCardView(cardView: View, textViewId: Int) {
+    private fun setupCardView(cardView: View, textView: TextView, index: Int) {
         cardView.setBackgroundResource(R.drawable.card_background)
-        val textView = cardView.findViewById<TextView>(textViewId)
         textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.defaultGreen))
         cardView.setOnClickListener {
-            toggleSelection(cardView, textView)
+            toggleSelection(cardView, textView, index)
         }
     }
 
-    private fun toggleSelection(cardView: View, textView: TextView) {
-        val isSelected = cardView.tag as? Boolean ?: false
-        if (isSelected) {
-            cardView.setBackgroundResource(R.drawable.card_background)
-            textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.defaultGreen))
-        } else {
+    private fun toggleSelection(cardView: View, textView: TextView, index: Int) {
+        isSelectedArray[index] = !isSelectedArray[index]
+
+        if (isSelectedArray[index]) {
             cardView.setBackgroundResource(R.drawable.card_selected_background)
             textView.setTextColor(ContextCompat.getColor(requireContext(), android.R.color.white))
+        } else {
+            cardView.setBackgroundResource(R.drawable.card_background)
+            textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.defaultGreen))
         }
-        cardView.tag = !isSelected
     }
 }
