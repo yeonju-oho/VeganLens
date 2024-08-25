cc583be6f54bd74608764a975af153c105278ce5
d0ye <ehdp001123@gmail.com>
2024-08-25 14:20:00 +0900

    Update MyPageActivity.kt
    폴더 위치 변경함

--- app/src/main/java/com/example/veganlens/MyPageActivity.kt
+++ app/src/main/java/com/example/veganlens/MyPageActivity.kt
@@ -2,7 +2,6 @@ package com.example.veganlens
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.ImageView
