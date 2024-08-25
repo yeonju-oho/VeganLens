f2981838d3295ee663c045e820951342e24e2c8e
d0ye <ehdp001123@gmail.com>
2024-08-25 16:18:20 +0900

    관리자 페이지 1차
    관리자 페이지 1차

---
+++ app/src/main/java/com/example/veganlens/adminPages/IngredientManagementActivity.kt
@@ -0,0 +1,34 @@
+import com.example.veganlens.R
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+class IngredientManagementActivity : AppCompatActivity() {
+
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager: ViewPager2
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ingredient_management)
+
+        tabLayout = findViewById(R.id.tabLayout)
+        viewPager = findViewById(R.id.viewPager)
+
+        val adapter = IngredientPagerAdapter(this)
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = when (position) {
+                0 -> "계란"
+                1 -> "우유"
+                2 -> "육류"
+                3 -> "생선"
+                else -> null
+            }
+        }.attach()
+    }
+}
