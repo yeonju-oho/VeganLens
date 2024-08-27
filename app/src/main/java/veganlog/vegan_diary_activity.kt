bbcdc9eca34ec008142438c89aaa957c03e805b6
김윤진 <113752012+Kimyounjin@users.noreply.github.com>
2024-08-27 19:57:40 +0900

    비건로그
    비건로그 만들기

---
+++ app/src/main/java/veganlog/vegan_diary_activity.kt
@@ -0,0 +1,12 @@
+package com.example.veganlens
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class VeganDiaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vegan_diary)
+    }
+}
