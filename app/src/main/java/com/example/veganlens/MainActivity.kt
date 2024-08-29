f03fcd64fa01cac8f4cb8ef2db97a0c25ade62d2
yeonju-oho <yeonju.oho@gmail.com>
2024-08-29 19:56:10 +0900

    회원가입 한번만 되도록 수정

--- app/src/main/java/com/example/veganlens/MainActivity.kt
+++ app/src/main/java/com/example/veganlens/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.veganlens
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.veganlens.startPages.SplashActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
